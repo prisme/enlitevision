386ff9433f8b1cb8ed6ad4cae69e694504797ba9
jsaintprix <jeremy.saintprix@gmail.com>
2016-07-30 19:45:54 +0200

    tweak anim

--- index.js
+++ index.js
@@ -9,21 +9,25 @@ var split = require('./SplitText')
 TweenMax.globalTimeScale(1.0)
 var timeline = new TimelineMax({paused: true})
 
+var splash = document.querySelector('.featured .splash')
+timeline.to(splash, 0.8, {alpha: 1})
+
 var logo = document.querySelector('.logo img')
-timeline.set(logo, {y: -150, alpha: 0})
-timeline.to( logo, 0.8, {
+TweenLite.set(logo, {y: -150, alpha: 0})
+var tweenLogo = TweenLite.to(logo, 0.8, {
     y: 0,
     alpha : 1,
     ease: Power1.easeOut,
     force3D:true,
     clearProps: 'all'
 })
+timeline.add( tweenLogo, 0.4)
 
 
 var title = document.querySelector('.title')
 var titleText = new split('.title', {type: "words, chars"})
-timeline.set(title, {alpha:1})
-timeline.staggerFrom(titleText.words, 2.0, { alpha:0, rotationY:-10, rotationX: -20 }, 0.3)
+TweenLite.set(title, {alpha:1})
+timeline.staggerFrom(titleText.words, 2.0, { alpha:0, rotationY:-12, rotationX: -20 }, 0.3)
 
 
 
