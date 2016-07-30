0ba7500cd0a477da241ed577a195c41ee29c9b29
jsaintprix <jeremy.saintprix@gmail.com>
2016-07-30 18:29:54 +0200

    rough resize

--- index.js
+++ index.js
@@ -23,9 +23,26 @@ timeline.to( logo, 0.8, {
 var title = document.querySelector('.title')
 var titleText = new split('.title', {type: "words, chars"})
 timeline.set(title, {alpha:1})
-
 timeline.staggerFrom(titleText.words, 2.0, { alpha:0, rotationY:-10, rotationX: -20 }, 0.3)
 
 
 
 timeline.play()
+
+
+// Resize
+var portrait = false
+var ratio = 1.3
+
+var resizeHandler = function(){
+    portrait = (window.innerWidth / window.innerHeight) < ratio
+
+    flag = portrait ? 'portrait' : 'landscape'
+    if(document.body.classList.contains(flag)) return
+
+    document.body.classList= []
+    document.body.classList.add(flag)
+}
+
+window.addEventListener('resize', resizeHandler)
+resizeHandler()
